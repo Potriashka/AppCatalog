becf8f289856b57d07cb06f40cee94659ceae647
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-06 01:01:23 +0300

    updated application.py

--- application.py
+++ application.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/favicon.ico")
+def icon():
+    return send_from_directory(os.path.join(app.root_path, "static/images"),
+                               "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
 if __name__ == "__main__":
 	app.run(debug=True)
