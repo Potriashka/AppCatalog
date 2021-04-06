a8dd30b7041c2e5a309921b6e3c7eb4380e4f506
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-07 01:04:07 +0300

    updated base.tsx

--- static/scripts/base.tsx
+++ static/scripts/base.tsx
@@ -20,3 +20,34 @@ document.getElementById("signinButton").onclick = () => {
 document.getElementById("registerButton").onclick = () => {
     window.location.replace("/register");
 }
+
+function setCookie(name, value, exdays) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(";");
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+        return true;
+    } else {
+        return false;
+    }
+}
