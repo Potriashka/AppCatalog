e0f008fd63c56f12dc7d4c56244327038aabf3a4
Peter <petrpotriashka2@gmail.com>
2021-04-06 13:04:58 +0300

    updated about

--- application.py
+++ application.py
@@ -13,5 +13,9 @@ def icon():
     return send_from_directory(os.path.join(app.root_path, "static/images"),
                                "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
 	app.run(debug=True)
