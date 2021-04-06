3a4176d63ce4261b9bea3f3f18cbe51996db159d
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-06 17:41:07 +0300

    updated application.py

--- application.py
+++ application.py
@@ -17,5 +17,9 @@ def icon():
 def about():
     return render_template("about.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
