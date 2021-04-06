97e73d1d97091a2c98ed943f8e1c19c587ad7f6b
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-06 18:56:14 +0300

    updated register.tsx

--- static/scripts/register.tsx
+++ static/scripts/register.tsx
@@ -12,24 +12,32 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 function register() {
+    let done = false;
     let username = document.getElementById("username").value;
     let name = document.getElementById("name").value;
     let password1 = document.getElementById("password1").value;
     let password2 = document.getElementById("password2").value;
     if (password1 === password2) {
-        firebase.database().ref("/user/" + username).on("value", function(snapshot) {
-            if (!snapshot.val()) {
-                firebase.database().ref("/user/" + username).set({
-                    Username: username,
-                    Name: name,
-                    Password: password1
-                });
-            } else {
-                alert("A user with that username already exists! Try a different username.")
-                document.getElementById("username").value = "";
+        firebase.database().ref("/").on("value", function (snapshot) {
+            if (snapshot.val()) {
+                if (snapshot.val()["user"][username] == undefined) {
+                    done = true;
+                    firebase.database().ref("/user/" + username).set({
+                        Username: username,
+                        Name: name,
+                        Password: password1
+                    });
+                    window.location.replace("/");
+                    return;
+                } else {
+                    if (!done) {
+                        alert("A user with that username already exists! Try a different username.")
+                        document.getElementById("username").value = "";
+                    }
+                }
             }
         });
     } else {
-        alert("Passwords do not match! Try again!");
+        alert("Passwords do not match! Please, try again.");
     }
 }
