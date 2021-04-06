643334e7c5c3551287b033793c45c8348606cafd
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-06 20:55:39 +0300

    added another check (just to make sure that everything will be working)

--- static/scripts/register.tsx
+++ static/scripts/register.tsx
@@ -17,29 +17,33 @@ function register() {
     let name = document.getElementById("name").value;
     let password1 = document.getElementById("password1").value;
     let password2 = document.getElementById("password2").value;
-    if (password1 === password2) {
-        firebase.database().ref("/").on("value", function (snapshot) {
-            if (snapshot.val()) {
-                if (snapshot.val()["user"][username] == undefined) {
-                    done = true;
-                    firebase.database().ref("/user/" + username).set({
-                        Username: username,
-                        Name: name,
-                        Password: password1
-                    });
-                    window.sessionStorage.setItem("username", username);
-                    window.sessionStorage.setItem("name", name);
-                    window.location.replace("/");
-                    return;
-                } else {
-                    if (!done) {
-                        alert("A user with that username already exists! Try a different username.")
-                        document.getElementById("username").value = "";
+    if (username !== "" && name != "" && password1 != "" && password2 != ""){
+        if (password1 === password2) {
+            firebase.database().ref("/").on("value", function (snapshot) {
+                if (snapshot.val()) {
+                    if (snapshot.val()["user"][username] == undefined) {
+                        done = true;
+                        firebase.database().ref("/user/" + username).set({
+                            Username: username,
+                            Name: name,
+                            Password: password1
+                        });
+                        window.sessionStorage.setItem("username", username);
+                        window.sessionStorage.setItem("name", name);
+                        window.location.replace("/");
+                        return;
+                    } else {
+                        if (!done) {
+                            alert("A user with that username already exists! Try a different username.")
+                            document.getElementById("username").value = "";
+                        }
                     }
                 }
-            }
-        });
+            });
+        } else {
+            alert("Passwords do not match! Please, try again.");
+        }
     } else {
-        alert("Passwords do not match! Please, try again.");
+        alert("You should fill in all input fields!");
     }
 }
