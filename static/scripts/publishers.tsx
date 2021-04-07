8169bfd13e094b3158a71484c56e5b4f59a94920
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-07 14:40:29 +0300

    updated publishers.tsx

--- static/scripts/publishers.tsx
+++ static/scripts/publishers.tsx
@@ -2,10 +2,12 @@ let container = document.getElementById("publishers");
 
 firebase.database().ref("/").on("value", function(snapshot) {
     if (snapshot.val()) {
+        let publishers = "";
         for (let username in snapshot.val()["user"])
         {
-            container.innerHTML += snapshot.val()["user"][username]["Name"] + "<br>";
+            publishers += `${snapshot.val()["user"][username]["Name"]}<br>`;
         }
+        container.innerHTML = publishers;
     } else {
         container.innerHTML = "<h2>There are no publishers yet</h2>"
     }
