00d26f9634e0becdd0278980a13b1218e36a268d
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-07 14:54:58 +0300

    updated register.tsx

--- static/scripts/register.tsx
+++ static/scripts/register.tsx
@@ -1,4 +1,5 @@
 function register() {
+    document.getElementById("registerButton").innerHTML = "<i class=\"fa fa-spinner fa-spin\"></i> Register";
     let done = false;
     let username = document.getElementById("username").value;
     let name = document.getElementById("name").value;
