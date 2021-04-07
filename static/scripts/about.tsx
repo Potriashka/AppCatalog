2f68ffaebd2ad638cdb84ef6e62869ae63ed5a15
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-07 14:30:37 +0300

    updated about.tsx

--- static/scripts/about.tsx
+++ static/scripts/about.tsx
@@ -1,17 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCILHhUb6UyKgCNDp1Gbav5Y4HwaDMZvWM",
-    authDomain: "appcatalog-6662e.firebaseapp.com",
-    projectId: "appcatalog-6662e",
-    storageBucket: "appcatalog-6662e.appspot.com",
-    messagingSenderId: "966730374376",
-    appId: "1:966730374376:web:eaa3204befa2cbae5ffd5d",
-    measurementId: "G-6N1E3265JP"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-
 function test1() {
     // getting the input by the id
     let test = document.getElementById("test").value;
