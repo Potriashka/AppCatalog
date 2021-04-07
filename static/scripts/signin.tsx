cf40faba268f6e52bcab5dfabec81c8b0c928ca0
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-07 14:32:06 +0300

    updated signin.tsx

--- static/scripts/signin.tsx
+++ static/scripts/signin.tsx
@@ -1,16 +1,3 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyCiu_etP0l4eqawwQvwKRlbmoRdGtMxrAM",
-    authDomain: "fir-26084.firebaseapp.com",
-    projectId: "fir-26084",
-    storageBucket: "fir-26084.appspot.com",
-    messagingSenderId: "500541767729",
-    appId: "1:500541767729:web:c2ce6ae2f296c6db351510",
-    measurementId: "G-ZWPSNE451F"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
 function signin() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
