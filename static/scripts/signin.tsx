381df0bf1bb887ab27fc5c971ef2d4da5f3e8af4
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-08 20:18:19 +0300

    fixed a bug

--- static/scripts/signin.tsx
+++ static/scripts/signin.tsx
@@ -1,19 +1,25 @@
 function signin() {
-    document.getElementById("buttonSignIn").innerHTML = "<i class=\"fa fa-spinner fa-spin\"></i> Sign In";
+    let button = document.getElementById("buttonSignIn");
+    button.innerHTML = "<i class=\"fa fa-spinner fa-spin\"></i> Sign in";
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
     if (username == "" || password == "") {
+        button.innerHTML = "Sign in";
         alert("You must fill in all input fields!")
     } else {
         firebase.database().ref("/user/" + username).on("value", function(snapshot) {
             if (snapshot.val() == undefined) {
-                alert("Username or password is incorrect. Try again");
+                button.innerHTML = "Sign In";
+                alert("Username or password is incorrect. Try again.");
             } else {
                 if (snapshot.val()["Password"] == password) {
                     setCookie("username", username, 100);
                     setCookie("name", snapshot.val()["Name"], 100);
                     window.location.replace("/");
+                } else {
+                    button.innerHTML = "Sign in";
+                    alert("Username or password is incorrect. Try again.");
                 }
             }
         });
