1ef9a2802bf9e892501c042577446ac0eed04108
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-17 17:22:04 +0300

    removed about.tsx

--- static/scripts/about.tsx
+++
@@ -1,8 +0,0 @@
-function test1() {
-    // getting the input by the id
-    let test = document.getElementById("test").value;
-    // setting the value
-    firebase.database().ref("/user/"+test).set({
-        Name: test
-    });
-}
