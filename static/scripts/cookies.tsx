4f83ca5d2f5f1f38c2b43a6cad68a22c29ee3a06
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-04-17 17:47:32 +0300

    added cookies.tsx

---
+++ static/scripts/cookies.tsx
@@ -0,0 +1,26 @@
+let setCookie = (name, value, exdays) => {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+let getCookie = (cname) => {
+    let name = cname + "=";
+    let ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+let checkCookie = (cookie) => {
+    let c = getCookie(cookie);
+    return c != "";
+}
