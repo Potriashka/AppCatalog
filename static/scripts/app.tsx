a93d2291a19092b679c122662ab1effa100eb62b
Peter <petrpotriashka2@gmail.com>
2021-04-18 15:34:36 +0300

    unfinished app page

--- static/scripts/app.tsx
+++ static/scripts/app.tsx
@@ -1,5 +1,6 @@
 let name = document.getElementById("publisherName").innerText;
 let app;
+let img;
 let found = false;
 
 firebase.database().ref("/user/").on("value", (snapshot) => {
@@ -9,16 +10,25 @@ firebase.database().ref("/user/").on("value", (snapshot) => {
             app = val["apps"][document.getElementById("nameOfApp").innerText];
             break;
         }
+
     }
     if (!found) {
         alert("This app does not exist!");
     } else {
         let container = document.getElementById("appInfo");
+        let descContainer = document.getElementById("description");
+        let link = document.getElementById("link");
+        let type = document.getElementById("type");
+        let source = document.getElementById("source");
         container.innerHTML = app["appName"];
+        descContainer.innerHTML = app["description"];
+        link.innerHTML = app["link"];
+        type.innerHTML = app["appType"];
+        source.innerHTML = app["source"];
+        let link2 = document.createElement("a");
+        a.appendChild('https://' + link);
         // app["appType"] for type
-        // app["description"] for description
         // app["imageUrl"] for image link
-        // app["link"] for link to the website
         // app["source"] for link to the source code
     }
 });
