ad5b3189f8ad042a7a73c2447a4987ff3ece9807
Peter <petrpotriashka2@gmail.com>
2021-06-03 02:18:45 +0300

    update that has to do good thing, but because of the bugs it makes everything even worse

--- static/scripts/publisher.tsx
+++ static/scripts/publisher.tsx
@@ -3,6 +3,12 @@ let name = document.getElementById("publisherName").innerText;
 let content = document.getElementById("contentDiv");
 let apps;
 let vers;
+let publisherNameCookies = getCookie("name");
+let bool = checkCookie("name");
+
+// for debugging
+console.log(bool);
+console.log(bool.toString() == "false");
 
 firebase.database().ref("/user/").on("value", (snapshot) => {
     console.log(snapshot.val());
@@ -18,12 +24,41 @@ firebase.database().ref("/user/").on("value", (snapshot) => {
         alert("This publisher does not exist!");
     } else {
         for(const [key, val] of Object.entries(apps)) {
-            if (vers) {
-                content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a><span class="text-muted">✓</span></span><br>`;
+            if (publisherNameCookies == name) {
+                console.log("Everything's OK!", publisherNameCookies);
+                if (vers) {
+                    content.innerHTML += `<btn class="cross" type="button">✕</btn><a href="/publisher/${name}/${val["appName"]}" class="b"> ${val["appName"]}</a><span class="text-muted">✓</span></span><br>`;
+                }
+                else {
+                    content.innerHTML += `<btn class="cross" type="button">✕</btn><a href="/publisher/${name}/${val["appName"]}" class="b"> ${val["appName"]}</a></span><br>`;
+                }
+            if (bool.toString() == "false") { // not "bool == false" because of debugging
+                console.log("Nope!", publisherNameCookies);
+                if (vers) {
+                    content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a><span class="text-muted">✓</span></span><br>`;
+                }
+                else {
+                    content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a></span><br>`;
+                }
             }
-            else {
-                content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a></span><br>`;
+            if (publisherNameCookies != name) {
+                console.log("Nope!", publisherNameCookies);
+                if (vers) {
+                    content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a><span class="text-muted">✓</span></span><br>`;
+                }
+                else {
+                    content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a></span><br>`;
+                }
+            }
             }
         }
     }
 });
+
+let appName = // somehow need to find the name of the app, where the cross is ¯\_(ツ)_/¯
+
+// this part doesn't work at all for some reason
+document.getElementsByClassName("cross").onclick = () => {
+    console.log("Pressed!"); // even this
+    firebase.database().ref('/user/' + name + appName).remove();
+}
