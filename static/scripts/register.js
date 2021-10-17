fefddcb2ab5e58408d7f05efb7ab23e3798c6d89
Peter <petrpotriashka2@gmail.com>
2021-10-17 15:52:10 +0300

    updated the register form

--- static/scripts/register.js
+++ static/scripts/register.js
@@ -10,32 +10,38 @@ let register = () => {
     let name = document.getElementById("name").value;
     let password1 = document.getElementById("password1").value;
     let password2 = document.getElementById("password2").value;
+    let checkbox = document.getElementById("checkboxReg");
     if (username !== "" || name != "" || password1 != "" || password2 != "") {
         if (password1 === password2) {
-            firebase.database().ref("/").on("value", function(snapshot) {
-                if (snapshot.val()) {
-                    if (snapshot.val()["user"][username] == undefined) {
-                        done = true;
-                        firebase.database().ref("/user/" + username).set({
-                            Username: username,
-                            Name: name,
-                            Password: password1
-                        });
-                        setCookie("username", username, 100);
-                        setCookie("name", name, 100);
-                        button.innerHTML = "Register";
-                        closeDialog();
-                        organizeButtons();
-                        return;
-                    } else {
-                        if (!done) {
+            if (checkbox.checked) {
+                firebase.database().ref("/").on("value", function (snapshot) {
+                    if (snapshot.val()) {
+                        if (snapshot.val()["user"][username] == undefined) {
+                            done = true;
+                            firebase.database().ref("/user/" + username).set({
+                                Username: username,
+                                Name: name,
+                                Password: password1
+                            });
+                            setCookie("username", username, 100);
+                            setCookie("name", name, 100);
                             button.innerHTML = "Register";
-                            alert("A user with that username already exists! Try a different username.")
-                            document.getElementById("username").value = "";
+                            closeDialog();
+                            organizeButtons();
+                            return;
+                        } else {
+                            if (!done) {
+                                button.innerHTML = "Register";
+                                alert("A user with that username already exists! Try a different username.")
+                                document.getElementById("username").value = "";
+                            }
                         }
                     }
-                }
-            });
+                });
+            } else {
+                button.innerHTML = "Register";
+                alert("You haven't accepted cookies! Please, try again.");
+            }
         } else {
             button.innerHTML = "Register";
             alert("Passwords do not match! Please, try again.");
