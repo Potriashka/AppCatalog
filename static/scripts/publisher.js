2639e28ec5d9c9953e95c8cf3a3bc5058deef31d
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2022-07-14 22:38:40 +0500

    updated

--- static/scripts/publisher.js
+++ static/scripts/publisher.js
@@ -1,13 +1,12 @@
 let found_publisher = false;
-let name = document.getElementById("publisherName").innerText;
+let publisherName = document.getElementById("publisherName").innerText;
 let content = document.getElementById("contentDiv");
 let apps;
 let vers;
 
 firebase.database().ref("/user/").on("value", (snapshot) => {
-    console.log(snapshot.val());
-    for (const [key, val] of Object.entries(snapshot.val())) {
-        if (val["Name"] == name) {
+    for (const [, val] of Object.entries(snapshot.val())) {
+        if (val["Name"] == publisherName) {
             apps = val["apps"];
             vers = val["verified"];
             found_publisher = true;
@@ -17,12 +16,12 @@ firebase.database().ref("/user/").on("value", (snapshot) => {
     if (!found_publisher) {
         alert("This publisher does not exist!");
     } else {
-        for (const [key, val] of Object.entries(apps)) {
+        for (const [, val] of Object.entries(apps)) {
             if (vers) {
-                content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a><span class="text-muted">✓</span></span><br>`;
+                content.innerHTML += `<a href="/publisher/${publisherName}/${val["appName"]}" class="b">${val["appName"]}</a><span class="text-muted">✓</span></span><br>`;
             } else {
-                content.innerHTML += `<a href="/publisher/${name}/${val["appName"]}" class="b">${val["appName"]}</a></span><br>`;
+                content.innerHTML += `<a href="/publisher/${publisherName}/${val["appName"]}" class="b">${val["appName"]}</a></span><br>`;
             }
         }
     }
-})
+});
